c6bb11029e73e1df9d94772046a245d54906d9fe
Arnaud <arnaud.a.Durand@gmail.com>
2022-10-06 09:34:33 +0300

    first commit - wkg

---
+++ server/helper.js
@@ -0,0 +1,6 @@
+function empty0rRows(rows){
+    if (!rows) return []
+    return rows
+}
+
+module.exports = {empty0rRows}
--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const mysql = require("mysql2/promise");
-const config = require("./config");
+// const mysql = require("mysql2/promise");
+// const config = require("./config");
+// const db = require("./services/db");
+const todo = require("./services/todo");
 
 const app = express();
 
@@ -13,8 +15,11 @@ const port = 3001;
 
 app.get("/", async function (req, res) {
   try {
-    const connection = await mysql.createConnection(config.db);
-    const [result] = await connection.execute("select * from task");
+    // const connection = await mysql.createConnection(config.db);
+    // const [result] = await connection.execute("select * from task");
+    // const result = await db.query("select * from task");
+    const result = await todo.getAllTasks();
+    if (!result) result = [];
     res.status(200).json(result);
   } catch (err) {
     res.status(500).send(err.message);
@@ -23,12 +28,16 @@ app.get("/", async function (req, res) {
 
 app.post("/new", async function (req, res) {
   try {
-    const connection = await mysql.createConnection(config.db);
-    const [result] = await connection.execute(
-      "insert into task (description) values (?)",
-      [req.body.description]
-    );
-    res.status(200).json({ id: result.insertId });
+    // const connection = await mysql.createConnection(config.db);
+    // const [result] = await connection.execute(
+    //   "insert into task (description) values (?)",
+    //   [req.body.description]
+    // );
+    // const result = await db.query("insert into task (description) values (?)", [
+    //   req.body.description,
+    // ]);
+    const result = await todo.addTask(req.body);
+    res.status(200).json({ id: result.id });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
@@ -36,8 +45,12 @@ app.post("/new", async function (req, res) {
 
 app.delete("/delete/:id", async function (req, res) {
   try {
-    const connection = await mysql.createConnection(config.db);
-    await connection.execute("delete from task where id = ?", [req.params.id]);
+    // const connection = await mysql.createConnection(config.db);
+    // await connection.execute("delete from task where id = ?", [req.params.id]);
+    // const result = await db.query("delete from task where id = ?", [
+    //   req.params.id,
+    // ]);
+    await todo.removeTask(req.params.id);
     res.status(200).json({ id: req.params.id });
   } catch (err) {
     res.status(500).json({ error: err.message });
