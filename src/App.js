5b67f028df267af60bf1910f21c244b0f58e921c
Arnaud <arnaud.a.Durand@gmail.com>
2022-10-06 08:48:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,26 +2,74 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const URL = "http://localhost:3001/"
+const URL = "http://localhost:3001/";
 
 function App() {
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState("");
 
   useEffect(() => {
-    axios.get(URL)
+    axios
+      .get(URL)
       .then((response) => {
         setTasks(response.data);
-      }).catch((error) => {
+      })
+      .catch((error) => {
         alert(error.reponse.data.error);
       });
   }, []);
 
+  function save() {
+    const json = JSON.stringify({ description: task });
+    axios
+      .post(URL + "new", json, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        const addedObject = JSON.parse(json);
+        addedObject.id = response.data.idsetTasks((tasks) => [
+          ...tasks,
+          addedObject,
+        ]);
+        setTask("");
+      })
+      .catch((error) => {
+        alert(error.response.data.error);
+      });
+  }
+
+  function remove(id) {
+    axios
+      .delete(`${URL}delete/${id}`)
+      .then(() => {
+        const newListWithoutRemoved = tasks.filter((item) => item.id !== id);
+        setTasks(newListWithoutRemoved);
+      })
+      .catch((error) => {
+        alert(error.reponse.data.error);
+      });
+  }
+
   return (
     <div>
       <h3>My Tasks</h3>
+      <form>
+        <label>Add new</label>
+        <input value={task} onChange={(e) => setTask(e.target.value)} />
+        <button type="button" onClick={save}>
+          Save
+        </button>
+      </form>
       <ol>
         {tasks.map((task) => (
-          <li key={task.id}>{task.description}</li>
+          <li key={task.id}>
+            {task.description}
+            <a href="#" onClick={() => remove(task.id)}>
+              Delete
+            </a>
+          </li>
         ))}
       </ol>
     </div>
